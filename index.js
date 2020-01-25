4ed95b75630e0ae12b9968491eae9589324435c5
IlyaMatsuev <matsuev2000@mail.ru>
2020-01-26 01:17:41 +0300

    Decided to use http protocol instead of https due to heroku limitations

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const https = require('https');
-const { readFileSync } = require('fs');
 
-const httpsConfig = require('./config').server;
-const httpsCredentials = {
-    key: readFileSync('certs/server.key', 'utf8'),
-    cert: readFileSync('certs/server.cert', 'utf8')
-};
+const serverConfig = require('./config').server;
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
-
-https.createServer(httpsCredentials, app).listen(httpsConfig.port, httpsConfig.host, () => {
-    console.log(`Listening to https://${httpsConfig.host}:${httpsConfig.port}/`);
+app.listen(process.env.PORT || serverConfig.port, () => {
+    console.log(`Listening to http://${serverConfig.host}:${serverConfig.port}/`);
 });
